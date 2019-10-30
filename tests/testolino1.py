a62475e08be2aa52516f7d3ea833500e211b18b0
mava70 <valentinimarko@gmail.com>
2019-10-30 20:57:20 +0100

    changed for env and docker compose

--- tests/testolino1.py
+++ tests/testolino1.py
@@ -3,6 +3,7 @@ import sys
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 url=sys.argv[1]
+print("Testolino is running")
 driver = webdriver.Chrome('chromedriver')  # Optional argument, if not specified will search path.
 driver.get('http://'+ url+ ':5000/');
 time.sleep(1) # Let the user actually see something!
@@ -14,3 +15,4 @@ instance_box.send_keys( Keys.ENTER, Keys.ARROW_DOWN, Keys.ENTER )
 time.sleep(1) # Let the user actually see something!a
 table = driver.find_element_by_tag_name("table");
 driver.quit()
+print('testolino is ended without error')
