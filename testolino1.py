4c59aa019d444382dc660929a8a11eb9dd7bd1fb
mava70 <valentinimarko@gmail.com>
2019-10-30 17:24:27 +0100

    changed for env and docker compose

---
+++ testolino1.py
@@ -0,0 +1,16 @@
+import time
+import sys
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+url=sys.argv[1]
+driver = webdriver.Chrome('/terraform/chromedriver.exe')  # Optional argument, if not specified will search path.
+driver.get('http://'+ url+ ':5000/');
+time.sleep(1) # Let the user actually see something!
+region_box = driver.find_element_by_id('comboA')
+region_box.send_keys( Keys.ENTER, Keys.ARROW_DOWN, Keys.ENTER )
+time.sleep(1) # Let the user actually see something!a
+instance_box = driver.find_element_by_id('comboA')
+instance_box.send_keys( Keys.ENTER, Keys.ARROW_DOWN, Keys.ENTER )
+time.sleep(1) # Let the user actually see something!a
+table = driver.find_element_by_tag_name("table");
+driver.quit()
